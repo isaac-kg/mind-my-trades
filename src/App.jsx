d085fb2ba849cd5e97434701ea95e457fdb04a1d
isaac-kg <isaackagiso7@gmail.com>
2023-04-03 20:52:52 +0200

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Button from './common/Button'
 import Input from './common/Input'
+import Login from './pages'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Button name="submit" color="pink"  onClick={() => console.log("This will show pink")}/>
-      <Button name="submit" color="green"  onClick={() => console.log("This will show green")}/>
-      <Button name="submit" color="blue"  onClick={() => console.log("This will show blue")}/>
-      <Input />
+      <Login />
     </div>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Input from "../common/Input";
+import Button from "../common/Button";
 
 const Login = () => {
 
@@ -8,16 +9,36 @@ const Login = () => {
 
   return (
     <React.Fragment>
-      <Input 
-        name="username"
-        label={"User Name"}
-        value={username}
-      />
-      <Input
-        name="password" 
-        label={"Password"}
-        value={password}
-      />
+      <div style={{
+        maxWidth: "300px",
+        background: "rgb(244 244 244)",
+        padding: "40px 20px",
+        borderRadius: "10px",
+        margin: "auto"
+      }}>
+        <Input 
+          name="username"
+          label={"User Name"}
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <Input
+          name="password" 
+          label={"Password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div
+          style={{
+            marginTop: "20px"
+          }}
+        >
+          <Button 
+            name="Submit"
+            color={"#040404"}
+          />
+        </div>
+      </div>
     </React.Fragment>
   )
  
