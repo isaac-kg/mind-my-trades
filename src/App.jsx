0f793d18f6b5104a5d0a61afa504c9fa74b51949
isaac-kg <isaackagiso7@gmail.com>
2023-07-31 21:32:07 +0200

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Button from './common/Button'
-import Input from './common/Input'
-import Login from './pages'
+import { useState } from "react";
+import "./App.css";
+import Button from "./common/Button";
+import Input from "./common/Input";
+import Login from "./pages";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline bg-black text-white font-white">Hello world!</h1>
       <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
