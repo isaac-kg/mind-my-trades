57562a968fed2e6f738e24023b5158b4d8dd2f10
isaac-kg <isaackagiso7@gmail.com>
2023-09-20 21:20:30 +0200

    ui fix

---
+++ src/jsForReact/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+const JSForReact = () => {
+
+
+  //Functions
+  const DoSomething = () => {
+
+  }
+
+  //Objects
+  const person = {
+    name: "Isaac",
+    age: 25,
+    isWorking: false
+  }
+
+  //destructing
+  const {name, age, isWorking} = person;
+
+
+  //Have to copy an object 
+  const person2 = {...person, name: "kagiso"}
+
+  //Hight Order Arrays
+  //.map and .filter
+ 
+
+  return (<div>
+
+  </div>)
+}
+
+export default JSForReact
