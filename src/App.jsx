750fb05ce7d368455f1122ea4ae9b7ba8d0b0369
isaac-kg <isaackagiso7@gmail.com>
2023-11-18 14:08:41 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ function App() {
 
   return (
     <div className="p-12">
-      <button className="bg-blue-400 text-white px-4 py-2 rounded-xl bg-gradient-to-r from-indigo-300 ">Get Started</button>
+      <div className="md:flex justify-center items-center">
+      <div className="basis-1/2">
+        <p className="font-semibold text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur architecto unde officiis.</p>
+        <p className="my-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti eaque sunt ipsum quo iusto aut.</p>
+        <button className="bg-blue-400 text-white px-8 py-2 rounded-xl bg-gradient-to-r from-indigo-300 ">Get Started</button>
+      </div>
+      <div className="hidden md:block basis-1/2">
+        ********
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat officia quia harum iusto cumque nisi, 
+        ea quibusdam nobis, dolorum sint tempore saepe maxime voluptatum. Atque facilis odit ullam corporis officia!
+        ********
+      </div>
+
+      </div>
       <div className="p-4 bg-red-400 rounded-xl mt-12">
         Lorem ipsum dolor sit,
         amet consectetur adipisicing elit. Doloremque nobis placeat vitae.
