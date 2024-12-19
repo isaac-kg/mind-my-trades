fe866eb8f192bcdd40805cfcd98278eaf30d9b1d
isaac-kg <isaackagiso7@gmail.com>
2024-12-19 22:43:14 +0200

    fix hero section

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
       <Hero />
       <About />
-      <LiveMarket />
+      {/* <LiveMarket /> */}
       <Footer />
     </div>
   )
--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,8 +1,21 @@
 const Hero = () => {
   return (
-    <div className="md:flex justify-center items-center h-screen relative">
-      <div className="basis-3/4 h-full bg-tn_blue_50 flex items-center">
-        <div>
+    <div className="lg:flex justify-center items-center h-screen relative">
+      <div className="basis-3/4 h-full bg-tn_blue_50 px-6 lg:pl-32 flex flex-col">
+        <div className="flex justify-between mt-6 gap-4 items-center">
+          <div>
+            <img
+              className="max-h-[50px]"
+              src="/icons/logo.svg"
+              alt=""
+              srcset=""
+            />
+          </div>
+          <button className="text-white py-2 px-5 bg-tn_green_50 border-tn_green_50 border-2 rounded-lg block lg:hidden">
+           Sign In
+          </button>
+        </div>
+        <div className="z-10 my-auto">
           <p className="text-white text-4xl">
             Lorem ipsum dolor sit amet consectetur.
           </p>
@@ -11,7 +24,7 @@ const Hero = () => {
           </p>
           <div>
             <button className="text-white py-3 px-5 bg-tn_green_50 border-tn_green_50 border-2 rounded-lg">
-              Start Now
+              Get Started
             </button>
             <button className="text-white py-3 px-5 border-tn_green_50 border-2  rounded-lg ml-8">
               Contact Us
@@ -19,9 +32,20 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="hidden md:block basis-1/4 h-full bg-tn_gray_50"></div>
-      <div className="absolute right-44">
-        <img className="max-h-[450px]" src="/icons/phone.svg" alt="" srcset="" />
+      <div className="hidden lg:flex basis-1/4 h-full bg-tn_gray_50 justify-end">
+        <div className="pr-32 mt-6">
+          <button className="text-white py-3 px-5 bg-tn_green_50 border-tn_green_50 border-2 rounded-lg">
+            Sign In
+          </button>
+        </div>
+      </div>
+      <div className="absolute right-0 hidden lg:block">
+        <img
+          className="max-h-[450px] lg:pr-32"
+          src="/icons/phone.svg"
+          alt=""
+          srcset=""
+        />
       </div>
     </div>
   )
